5d3b63fe449df72feaeac1057f4e0cda968333df
faazizpro <fahadafs11@gmail.com>
2022-09-01 17:57:22 +0600

    Complete module 33-3 And displayed country

--- js/countries.js
+++ js/countries.js
@@ -1,1 +1,24 @@
-console.log('Country connected');
+const loadCountries = () =>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => displayCountries(data));
+}
+
+const displayCountries = countries => {
+    // for(const country of countries){
+    //     console.log(country);
+    // }
+    const countriesContainer = document.getElementById('countries-container');
+    countries.forEach(country => {
+        console.log(country);
+        const countryDiv  = document.createElement('div');
+        countryDiv.classList.add('country');
+        countryDiv.innerHTML = `
+        <h3>Name: ${country.name.common}</h3>
+        <h4>Capital: ${country.capital ? country.capital[0] : 'No Capital'}</h4>
+        `;
+        countriesContainer.appendChild(countryDiv);
+    })
+}
+
+ loadCountries();
