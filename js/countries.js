763c48479915c36f7713962d7ce4b3c487f86054
faazizpro <fahadafs11@gmail.com>
2022-09-01 19:23:43 +0600

    completed module 33-4. It must be revised again

--- js/countries.js
+++ js/countries.js
@@ -5,20 +5,37 @@ const loadCountries = () =>{
 }
 
 const displayCountries = countries => {
-    // for(const country of countries){
-    //     console.log(country);
-    // }
+
     const countriesContainer = document.getElementById('countries-container');
     countries.forEach(country => {
-        console.log(country);
+        // console.log(country);
         const countryDiv  = document.createElement('div');
         countryDiv.classList.add('country');
         countryDiv.innerHTML = `
         <h3>Name: ${country.name.common}</h3>
         <h4>Capital: ${country.capital ? country.capital[0] : 'No Capital'}</h4>
+        <button onclick="loadCountryFlag('${country.cca2}')">Details</button>
         `;
         countriesContainer.appendChild(countryDiv);
     })
 }
+
+    const loadCountryFlag = (code) =>{
+        // https://restcountries.com/v3.1/alpha/{code}
+        const url = `https://restcountries.com/v3.1/alpha/${code}`
+        
+        console.log(url);
+        fetch (url)
+        .then (res => res.json())
+        .then (data => displayCountryDetail(data[0]))
+    }
+
+    const displayCountryDetail = country =>{
+        const countryDetail = document.getElementById('country-detail');
+        countryDetail.innerHTML = `
+        <h2>Country Name: ${country.name.common}</h2>
+        <img src="${country.flags.png}">
+        `;
+    }
 
  loadCountries();
